bbf577e36743bb31a7c71037e36b2991fc6f3e63
MATHEUS PEREIRA DE AGUIAR <25204557@dsn127m1247444.cfp127aluno.local>
2026-02-24 11:29:45 -0300

    style: adicionando tratamento e validacoes

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 'use strict'
 
-
-function adicionarCodigo(){
-       const lista = document.getElementById('lista')
-    const codigo = document.getElementById('codigo')
+    function adicionarCodigo(){
+        const lista = document.getElementById('lista')
+        const codigo = document.getElementById('codigo')
         const produto = document.getElementById('produto')
         const quantidade = document.getElementById('quantidade')
 
-    const span = document.createElement('span')
-    const span1 = document.createElement('span')
-    const span2 = document.createElement('span')
+        if(
+            produto == '' || codigo == '' || quantidade == ''
+        ){
+            return false
+        }else if(isNaN(produto) || isNaN(codigo) || isNaN(quantidade)){
+            return false
+        }
+        else{
+
+        const span = document.createElement('span')
+        const span1 = document.createElement('span')
+        const span2 = document.createElement('span')
 
 
-    span.textContent = produto.value
-    span1.textContent = codigo.value
-    span2.textContent = quantidade.value
+        span.textContent = produto.value
+        span1.textContent = codigo.value
+        span2.textContent = quantidade.value
 
-    span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
-    lista.append(span, span1, span2)
-
+        span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
+        span1.className = 'flex flex-col bg-yellow-100 px-8 py-2'
+        span2.className = 'flex flex-col bg-yellow-100 px-8 py-2'
+        lista.append(span, span1, span2)
+            return true
 }
-
+}
