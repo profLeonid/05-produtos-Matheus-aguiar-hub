70eb2096a2c6f27817b8b0c44e22a39f4306e4da
Matheus-aguiar-hub <marquinhos199613a@gmail.com>
2026-03-01 01:58:44 -0300

    Style: Adicionando bordas com blur, cor azul no body e função de verificação caso o usuario digite nulo, ou não é um número

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 'use strict'
 
     function adicionarCodigo(){
+
         const lista = document.getElementById('lista')
-        const codigo = document.getElementById('codigo')
-        const produto = document.getElementById('produto')
+        const codigo = document.getElementById('produto')
+        const produto = document.getElementById('codigo')
         const quantidade = document.getElementById('quantidade')
 
-        if(
-            produto == '' || codigo == '' || quantidade == ''
-        ){
-            return false
-        }else if(isNaN(produto) || isNaN(codigo) || isNaN(quantidade)){
-            return false
-        }
-        else{
+        const validando = tratandoLetras(
+            produto.value,
+            codigo.value,
+            quantidade.value
+        )
 
+         if (!validando) return true
+        
         const span = document.createElement('span')
         const span1 = document.createElement('span')
         const span2 = document.createElement('span')
 
-
-        span.textContent = produto.value
-        span1.textContent = codigo.value
+        span.textContent = codigo.value
+        span1.textContent = produto.value
         span2.textContent = quantidade.value
 
-        span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
-        span1.className = 'flex flex-col bg-yellow-100 px-8 py-2'
-        span2.className = 'flex flex-col bg-yellow-100 px-8 py-2'
-        lista.append(span, span1, span2)
-            return true
-}
+        span1.className = 'flex flex-col ; bg-gray-100 border rounded-xs ; px-8 py-4 '
+        span.className = 'flex flex-col ; bg-gray-100 border rounded-xs ; px-8 py-4'
+        span2.className = 'flex flex-col ; bg-gray-100 border rounded-xs ; px-8 py-4'
+
+        lista.append(span ,span1 , span2)
 }
--- verificacao.js
+++ verificacao.js
@@ -1,11 +1,14 @@
 function tratandoLetras(codigo, produto, quantidade ) {
     //Tratando caso o usuário digite nada
     if (codigo == '' || produto == '' || quantidade == ''  ) {
+        console.log('erro 1')
         return false
     //Tratando caso o usuário digite letras
-    } else if (isNaN(codigo) || isNaN(produto) || isNaN(quantidade)) {
+    } else if (isNaN(codigo) || isNaN(quantidade)) {
+        console.log('erro 2')
         return false
     } else {
+        console.log('certo')
         return true
     }
 }
