75cd2f3588a00d7f95ed9ea662ee2ea7c743c951
Matheus-aguiar-hub <marquinhos199613a@gmail.com>
2026-03-01 11:09:45 -0300

    feat: adicionando o botão de remover

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
     function adicionarCodigo(){
 
-        const lista = document.getElementById('lista')
-        const codigo = document.getElementById('produto')
-        const produto = document.getElementById('codigo')
-        const quantidade = document.getElementById('quantidade')
+        const lista         = document.getElementById('lista')
+        const codigo        = document.getElementById('produto')
+        const produto       = document.getElementById('codigo')
+        const quantidade    = document.getElementById('quantidade')
 
         const validando = tratandoLetras(
             produto.value,
@@ -15,22 +15,31 @@
 
          if (!validando) return true
         
-        const div = document.createElement('div')
-        const span = document.createElement('span' )
-        const span1 = document.createElement('span')
-        const span2 = document.createElement('span')
+        const div         = document.createElement('div')
+        const span        = document.createElement('span' )
+        const span1       = document.createElement('span')
+        const span2       = document.createElement('span')
 
-        
-        span.textContent = codigo.value
-        span1.textContent = produto.value
-        span2.textContent = quantidade.value
+        span.textContent      = codigo.value
+        span1.textContent     = produto.value
+        span2.textContent     = quantidade.value
 
-        div.className = 'space-y-4 mt-8'
-        span.className = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4 '
-        span1.className = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4'
-        span2.className = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4'
+        div.className       = 'space-y-4 mt-8'
+        span.className      = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4 '
+        span1.className     = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4'
+        span2.className     = 'flex flex-col ; bg-gray-100 border rounded-2xl ; px-8 py-4'
         
         div.append(span ,span1 , span2)
 
         lista.appendChild(div)
+}
+
+    function removerAnterior() {
+
+        const lista = document.getElementById('lista')
+
+        const ultimoElementoAcionado = lista.lastElementChild
+
+        if (ultimoElementoAcionado) ultimoElementoAcionado.remove()
+
 }
