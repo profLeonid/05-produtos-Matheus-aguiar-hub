c89b5ff083ca80ab4dddde268f130449f46ca385
Matheus-aguiar-hub <marquinhos199613a@gmail.com>
2026-03-02 15:19:19 -0300

    feat: arrumando bug da validação da letra por nome

--- verificacao.js
+++ verificacao.js
@@ -1,14 +1,14 @@
 function tratandoLetras(codigo, produto, quantidade ) {
     //Tratando caso o usuário digite nada
     if (codigo == '' || produto == '' || quantidade == ''  ) {
-        console.log('erro 1')
+        console.log('erro de caixa vazia')
         return false
     //Tratando caso o usuário digite letras
-    } else if (isNaN(codigo) || isNaN(quantidade)) {
-        console.log('erro 2')
+    } else if (isNaN(codigo) || isNaN(quantidade) || !isNaN(produto) ) {
+        console.log('erro de número ou letra')
         return false
     } else {
-        console.log('certo')
+        console.log('Validado')
         return true
     }
 }
